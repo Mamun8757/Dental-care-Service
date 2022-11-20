d50f227a2180a3ab8f8a77a05330313921c33c29
Mamun8757 <almamun8757@gmail.com>
2022-11-20 17:36:37 +0600

    Login Page Updated

--- src/Pages/LogIn/Login.js
+++ src/Pages/LogIn/Login.js
@@ -1,35 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div className="hero max-h-screen bg-base-200 py-10">
-            <div>
-                <div className="card w-full shadow-2xl bg-base-100">
-                    <div className="card-body">
-                    <div className="form-control">
-                        <label className="label">
-                        <span className="label-text">Email</span>
-                        </label>
-                        <input type="text" placeholder="email" className="input input-bordered" />
-                    </div>
-                        <div className="form-control">
-                            <label className="label">
-                            <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                            <Link to='/sign-up' className="label-text-alt link link-hover">Don't have account? Sign-Up Now</Link>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <Link to='/' className="btn headerBtn-color">LogIN</Link>
-                        </div>
-                    </div>
-                </div>
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+    }
+  return (
+    <div className="hero py-10 bg-slate-100 ">
+      <div className="hero-content flex-col">
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleSubmit} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
             </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <Link to="/sign-up" className="label-text-alt link link-hover">
+                  No Registration Yet? Sign-Up
+                </Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn headerBtn-color">Login</button>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
